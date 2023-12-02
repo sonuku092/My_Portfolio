1b50ad711fd8909685947942f47923896218810c
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2023-12-02 22:17:11 +0530

    Logo Updata

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="sonukumar" className="w-[124px] h-[32px]" />
+      <img src={logo} alt="sonukumar" className="w-auto h-[32px] cursor-pointer" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
