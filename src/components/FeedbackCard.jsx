81d459b496abb220048116f0e97a40d07123f4c2
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2023-12-10 02:07:04 +0530

    Done

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,9 +1,9 @@
 import { quotes } from "../assets";
 
 const FeedbackCard = ({ content, name, title, img }) => (
-  <div className="flex justify-between flex-col px-10 py-12 rounded-[20px]  max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
-    <img src={quotes} alt="double_quotes" className="w-[42.6px] h-[27.6px] object-contain" />
-    <p className="font-poppins font-normal text-[18px] leading-[32.4px] text-white my-10">
+  <div className="justify-between flex-col p-5 rounded-[18px] border-2  max-w-[320px] md:mr-10 sm:mr-5 mr-0 my-5">
+    <img src={quotes} alt="double_quotes" className="w-[32px] h-[32px] object-contain" />
+    <p className="font-poppins font-normal text-[18px] leading-[28.4px] text-white my-2">
       {content}
     </p>
 
