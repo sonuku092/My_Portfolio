07f6518f5894d13775f406333c62a7a5886f754d
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2023-12-29 13:14:48 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => (
       </div>
     </div>
     
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={` ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Skills/>
         {/* <Testimonials /> */}
