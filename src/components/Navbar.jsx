79380ed5ed3860aea96a99d225a6a0704c405df2
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-03-10 21:20:23 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,17 +56,17 @@ const Navbar = () => {
             !toggle ? "hidden" : "flex flex-col"
           } backdrop-blur-sm bg-black/90 absolute top-[60px] border-[1px] border-dimWhite right-0 mx-2 my-2 p-[4px] min-w-[140px] rounded-lg sidebar`}
         >
-          <h3 className="text-cyan-600 font-poppins font-normal border-b-[1px] border-black/20 text-center">Menu</h3>
+          <h3 className="font-poppins font-normal border-b-[1px] border-black/20 text-center text-white">Menu</h3>
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`font-poppins font-medium cursor-pointer text-[16px] w-full p-[4px] rounded-md ${
-                  active === nav.title ? "text-white bg-gradient-to-r from-cyan-500/70 to-blue-500/50" : "text-cyan-600 hover:text-white"
+                  active === nav.title ? " text-secondary bg-gradient-to-r from-slate-100/10 to-gray-50/20" : "text-white"
                 } ${index === navLinks.length - 1 ? "mb-0" : "mb-1"}`}
                 onClick={() => setActive(nav.title)}
               >
-                <a href={`#${nav.id}`}>{nav.title}</a>
+                <a href={`#${nav.id}`} className="flex">{nav.title}</a>
               </li>
             ))}
           </ul>
