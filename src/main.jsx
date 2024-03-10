27bdb20334a373c89b1fb3bfc66c59c7fcac286e
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-03-10 22:57:14 +0530

    UPDATE

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import Project from './pages/Project';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Project from "./pages/Project";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/project" element={<Project />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
