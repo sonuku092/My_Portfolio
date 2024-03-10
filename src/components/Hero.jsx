59aab2cb7a5dbb0c1defe64de2da45c724503cab
Sonu Kumar <1AT20IS092@visioneer.atria.edu>
2024-03-11 03:55:42 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,35 +3,58 @@ import { discount, robot, heroimg, Resume } from "../assets";
 import GetStarted from "./GetStarted";
 
 const Hero = () => {
-
   const downloadAndOpenCV = () => {
+    // Displaying an alert message while downloading
+    const alertDiv = document.createElement("div");
+    alertDiv.className =
+      " w-fit fixed top-1 left-0 right-0 mx-auto bg-green-500 text-bold text-white p-4 rounded shadow-lg";
+    alertDiv.innerText = "Downloading CV...";
+    document.body.appendChild(alertDiv);
+
     fetch(Resume)
-      .then(response => response.blob())
-      .then(blob => {
+      .then((response) => response.blob())
+      .then((blob) => {
         const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement('a');
+        const link = document.createElement("a");
         link.href = url;
-        link.setAttribute('download', 'SonuKumar_CV.pdf');
+        link.setAttribute("download", "SonuKumar_CV.pdf");
         document.body.appendChild(link);
         link.click();
         link.parentNode.removeChild(link);
-        window.open(Resume, "_blank"); // Open the downloaded file in a new tab
+        // window.open(url, '_blank'); // Open the downloaded PDF in a new tab
+        setTimeout(() => {
+          alertDiv.remove();
+        }, 1500); // Adjust the duration as needed (in milliseconds)
       })
-      .catch(error => {
-        console.error('Error downloading CV:', error);
+      .catch((error) => {
+        console.error("Error downloading CV:", error);
+        alertDiv.remove();
+        const errorAlertDiv = document.createElement("div");
+        errorAlertDiv.className =
+          "fixed top-0 left-0 right-0 mx-auto bg-red-500 text-white p-4 rounded shadow-lg";
+        errorAlertDiv.innerText = "Error downloading CV: " + error;
+        document.body.appendChild(errorAlertDiv);
+        setTimeout(() => {
+          errorAlertDiv.remove();
+        }, 1000); // Adjust the duration as needed (in milliseconds)
       });
-  }
+  };
 
   return (
-    <section id="profile" className={`flex md:flex-row flex-col sm:py-[100px] py-6 `}>
-      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 mt-10 xs:mt-5`}>
-        
+    <section
+      id="profile"
+      className={`flex md:flex-row flex-col sm:py-[100px] py-6 `}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 mt-10 xs:mt-5`}
+      >
         <div className="flex flex-row justify-between items-center w-full select-none">
           <h1 className="flex-1 font-Kalnia text-[18px] xs:text-[20px] ss:text-[24px] sm:text-[28px] md:text-[32px] text-white ss:leading-[55px] leading-[45px]">
-            Hello, I'm<br/>{" "}
+            Hello, I'm
+            <br />{" "}
             <span1 className=" name font-Racing text-[46px] xs:text-[52px] ss:text-[62px] sm:text-[72px] md:text-[92px] md:leading-[100px] font-bold">
               Sonu Kumar
-              </span1>{" "}
+            </span1>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
             <GetStarted />
@@ -39,32 +62,41 @@ const Hero = () => {
         </div>
 
         <h1 className="font-Kalnia font-normal text-[18px] xs:text-[24px] text-white select-none">
-          and I'm a <br className=" ss:hidden"/>
+          and I'm a <br className=" ss:hidden" />
           <span className=" ml-1 font-Squada font-semibold text-[36px] xs:text-[36px] ss:text-[48px] sm:text-[56px] md:text-[68px] ss:leading-[70px]  leading-[40px] w-full">
-           {" "}Software
-            </span>
-            <span className="ml-1 font-Squada font-semibold text-[36px] xs:text-[36px] ss:text-[48px] sm:text-[56px] md:text-[68px] ss:leading-[70px]  leading-[40px] w-full">
-           {" "}Developer
-            </span>
+            {" "}
+            Software
+          </span>
+          <span className="ml-1 font-Squada font-semibold text-[36px] xs:text-[36px] ss:text-[48px] sm:text-[56px] md:text-[68px] ss:leading-[70px]  leading-[40px] w-full">
+            {" "}
+            Developer
+          </span>
         </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5 ss:mb-10 mb-5 text-[16px] ss:text-lg`}>
-        Feel free to connect with me to discuss tech, share ideas, or explore
-        potential opportunities. Let's embark on a journey of innovation
-        together! 
+        <p
+          className={`${styles.paragraph} max-w-[470px] mt-5 ss:mb-10 mb-5 text-[16px] ss:text-lg`}
+        >
+          Feel free to connect with me to discuss tech, share ideas, or explore
+          potential opportunities. Let's embark on a journey of innovation
+          together!
         </p>
 
-        
-        <button className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 "
-          onClick={() => {downloadAndOpenCV();}}>
+        <button
+          className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2 "
+          onClick={downloadAndOpenCV}
+        >
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <a className={`${styles.paragraph} ml-2`}>
-            Download My CV
-          </a>
+          <p className={`${styles.paragraph} ml-2`}>Download My CV</p>
         </button>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} ss:my-0 my-10 relative`}>
-        <img src={heroimg} alt="billing" className="w-auto h-[100%] relative z-[5]" />
+      <div
+        className={`flex-1 flex ${styles.flexCenter} ss:my-0 my-10 relative`}
+      >
+        <img
+          src={heroimg}
+          alt="billing"
+          className="w-auto h-[100%] relative z-[5]"
+        />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
@@ -74,7 +106,7 @@ const Hero = () => {
       </div>
 
       <div className={`${styles.flexCenter}`}>
-        <GetStarted className />
+        <GetStarted />
       </div>
     </section>
   );
