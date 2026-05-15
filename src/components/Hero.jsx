1f9fe3ba85bda2481b1f98ed554285ad7d12d42a
sonuku092 <sonukumarmukhiya04@gmail.com>
2026-05-15 23:19:12 +0530

    Remove unnecessary text from Hero component's message

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -65,7 +65,7 @@ const Hero = () => {
         >
           Feel free to connect with me to discuss tech, share ideas, or explore
           potential opportunities. Let's embark on a journey of innovation
-          together!  From Offfice Branch
+          together!
         </p>
 
         <button
